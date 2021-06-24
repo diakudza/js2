d0b4bb823cc22082c2c8174cb8e4112a8a482a8b
diakudza <diakudza@gmail.com>
2021-06-24 11:33:47 +0300

    fix webpack

--- lesson8/webpack.config.js
+++ lesson8/webpack.config.js
@@ -1,6 +1,27 @@
+const CopyWebpack = require('copy-webpack-plugin')
+
 module.exports = {
   entry: "./src/app/script.js",
   output: {
     filename: "./script.js",
   },
+   plugins: [
+    new CopyWebpack ({
+        patterns: [
+            {
+                from: './src/public/img/',
+                to: './img',
+            },
+            {
+                from: './src/public/index.html',
+                to: './index.html',
+            },
+            {
+                from: './src/style/style.css',
+                to: './style.css',
+            },
+        ],
+    }),
+   ],
+
 };
